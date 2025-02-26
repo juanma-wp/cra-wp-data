4746468ccac6a2931925cb33698a73e2eadaec07
JuanMa Garrido <juanma.garrido@gmail.com>
2025-02-26 12:25:26 +0000

    fixed placeholder

--- src/components/product-display/CardProduct.js
+++ src/components/product-display/CardProduct.js
@@ -1,24 +1,36 @@
 import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
-} from 'reactstrap';
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
 
 const CardProduct = ({
   product = { name: "", description: "", price: "0.00" },
   addToCart,
-  inCart
+  inCart,
 }) => {
   const { name, description, price } = product;
 
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src="https://reactstrap.github.io/assets/318x180.svg" alt="Card image cap" />
+        <CardImg
+          top
+          width="100%"
+          src="https://placeholder.pics/svg/318x180"
+          alt="Card image cap"
+        />
         <CardBody>
           <CardTitle tag="h5">{name}</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">$ {price}</CardSubtitle>
-          <CardText>{ description }</CardText>
-          { !inCart && <Button onClick={addToCart}>Add to Cart</Button> }
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            $ {price}
+          </CardSubtitle>
+          <CardText>{description}</CardText>
+          {!inCart && <Button onClick={addToCart}>Add to Cart</Button>}
         </CardBody>
       </Card>
     </div>
